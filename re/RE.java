b828661cf040211506f5dde77404f4b61db49520
Trevor Smith <trevorsmith772@u.boisestate.edu>
2021-04-27 21:16:31 -0600

    Added RE class

---
+++ re/RE.java
@@ -0,0 +1,16 @@
+package re;
+
+import fa.State;
+import fa.dfa.DFA;
+import fa.nfa.NFA;
+import fa.nfa.NFAState;
+
+public class RE implements REInterface {
+
+    @Override
+    public NFA getNFA() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
