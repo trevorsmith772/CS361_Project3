9562a9915c17609a8b2d5da7bd6ee39be3906d5e
brandonmattaini <brandonmattaini@u.boisestate.edu>
2021-04-27 21:47:41 -0600

    Added some methods

--- re/RE.java
+++ re/RE.java
@@ -7,10 +7,50 @@ import fa.nfa.NFAState;
 
 public class RE implements REInterface {
 
+    private String regEx;
+    private int count;
+
+    public RE(String regEx) {
+        this.regEx = regEx;
+    }
+
     @Override
     public NFA getNFA() {
-        // TODO Auto-generated method stub
-        return null;
+        return regex();
     }
-    
+
+
+
+
+    private NFA regex() {
+        NFA term = term();
+
+        if (more() && peek() == '|') {
+            eat('|');
+            return union(term, regex());
+        }else{
+            return term;
+        }
+    }
+
+    private boolean more() {
+        return regEx.length() > 0;
+    }
+
+    private char peek() {
+        return regEx.charAt(0);
+    }
+
+    private void eat(char c) {
+        if (peek() == c){
+            regEx = regEx.substring(1);
+        }else{
+            throw new RuntimeException("Expected: " + c + "; got: " + peek());
+        }
+    }
+
+
+
+
+
 }
