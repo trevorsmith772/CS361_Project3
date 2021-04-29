14b74159fc1c435e4539fb72aacc0a9056ceff44
Trevor Smith <trevorsmith772@u.boisestate.edu>
2021-04-28 18:01:01 -0600

    Rearranged Methods into sections

--- re/RE.java
+++ re/RE.java
@@ -1,7 +1,6 @@
 package re;
 
 import fa.State;
-import fa.dfa.DFA;
 import fa.nfa.NFA;
 import fa.nfa.NFAState;
 
@@ -20,137 +19,140 @@ public class RE implements REInterface {
         return regex();
     }
 
-	private NFA base() {
-		switch (peek()) {
-		case '(':
-			eat('(');
-			NFA nfa = regex();
-			eat(')');
-			return nfa;
-
-		default:
-			return primitive(next());
-		}
-	}
-
-    private NFA factor() {
-		NFA base = base() ;
-		while (more() && peek() == '*') {
-			eat('*') ;
-			base =  repetition(base) ;
-		}
-		return base ;
-	}
-
-	private char next() {
-		char c = peek();
-		eat(c);
-		return c;
-	}
-
-    private NFA regex() {
-        NFA term = term();
-
-        if (more() && peek() == '|') {
-            eat('|');
-            return union(term, regex());
-        }else{
-            return term;
-        }
-    }
-
-    private boolean more() {
-        return regEx.length() > 0;
-    }
+    /* Recursive Descent Methods */
 
     private char peek() {
         return regEx.charAt(0);
     }
 
     private void eat(char c) {
-        if (peek() == c){
+        if (peek() == c) {
             regEx = regEx.substring(1);
-        }else{
+        } else {
             throw new RuntimeException("Expected: " + c + "; got: " + peek());
         }
     }
 
-	private NFA term() {
-		NFA factor = new NFA(); 
-		
-		factor.addStartState(Integer.toString(stateCount++));
-		String finalstate = Integer.toString(stateCount);
-		factor.addFinalState(Integer.toString(stateCount++));
-		factor.addTransition(factor.getStartState().getName(), 'e', finalstate);
+    private char next() {
+        char c = peek();
+        eat(c);
+        return c;
+    }
 
-		while (more() && peek() != ')' && peek() != '|') {
-			NFA nextFactor = factor();
-			factor = concatenate(factor, nextFactor); // factor = new Sequence(factor,nextFactor) ;
-		}
+    private boolean more() {
+        return regEx.length() > 0;
+    }
 
-		return factor;
-	}
+    /* Regular Expression Parsing Methods */
 
-    public NFA repetition(NFA base){
-		NFAState nfaState = (NFAState) base.getStartState();
-		for(State nfa : base.getFinalStates()){
-			base.addTransition(nfa.getName(), 'e', nfaState.getName());
-		}
+    private NFA regex() {
+        NFA term = term();
+        if (more() && peek() == '|') {
+            eat('|');
+            return union(term, regex());
+        } else {
+            return term;
+        }
+    }
 
-		String state = Integer.toString(stateCount++);
-		base.addStartState(state);
-		base.addFinalState(state);
-		base.addTransition(state, 'e', nfaState.getName());
-		return base;
-	}
+    private NFA term() {
+        NFA factor = new NFA();
 
-    public NFA primitive(char c){
-		NFA nfa = new NFA();
+        factor.addStartState(Integer.toString(stateCount++));
+        String finalstate = Integer.toString(stateCount);
+        factor.addFinalState(Integer.toString(stateCount++));
+        factor.addTransition(factor.getStartState().getName(), 'e', finalstate);
 
-		String startState = Integer.toString(stateCount++);
-		nfa.addStartState(startState);
+        while (more() && peek() != ')' && peek() != '|') {
+            NFA nextFactor = factor();
+            factor = concatenate(factor, nextFactor);
+        }
 
-		String finalState = Integer.toString(stateCount++);
-		nfa.addFinalState(finalState);
+        return factor;
+    }
 
-		nfa.addTransition(startState, c, finalState);
+    private NFA factor() {
+        NFA base = base();
+        while (more() && peek() == '*') {
+            eat('*');
+            base = repetition(base);
+        }
+        return base;
+    }
 
-		return nfa;
-	}
+    private NFA base() {
+        switch (peek()) {
+        case '(':
+            eat('(');
+            NFA nfa = regex();
+            eat(')');
+            return nfa;
 
-    public NFA concatenate(NFA nfa,NFA nfa2){
-		//System.out.println("concat");
-		nfa.addAbc(nfa2.getABC());
-		for(State s:nfa.getFinalStates()){
-			NFAState state = (NFAState)s;
-			state.setNonFinal();
-			state.addTransition( 'e', (NFAState)nfa2.getStartState());
-		}
-		nfa.addNFAStates(nfa2.getStates());
-		return nfa;
-	}
+        default:
+            return primitive(next());
+        }
+    }
 
-    public NFA union(NFA nfa, NFA nfa2){
-		//System.out.println("union");
-		NFAState nfaState = (NFAState) nfa.getStartState();
-		NFAState nfaState2 = (NFAState) nfa2.getStartState();
+    /* Additional Helper Methods */
 
-		nfa.addNFAStates(nfa2.getStates());
-		nfa.addAbc(nfa2.getABC());
+    public NFA repetition(NFA base) {
+        NFAState nfaState = (NFAState) base.getStartState();
+        for (State nfa : base.getFinalStates()) {
+            base.addTransition(nfa.getName(), 'e', nfaState.getName());
+        }
 
-		String startState = Integer.toString(stateCount++);
-		nfa.addStartState(startState);
-		String finalState = Integer.toString(stateCount++);
-		nfa.addFinalState(finalState);
+        String state = Integer.toString(stateCount++);
+        base.addStartState(state);
+        base.addFinalState(state);
+        base.addTransition(state, 'e', nfaState.getName());
+        return base;
+    }
 
-		nfa.addTransition(startState, 'e', nfaState.getName());
-		nfa.addTransition(startState, 'e', nfaState2.getName());  
+    public NFA primitive(char c) {
+        NFA nfa = new NFA();
 
-		for(State s:nfa2.getFinalStates()){
-			NFAState state = (NFAState)s;
-			state.setNonFinal();
-			nfa.addTransition(state.getName(), 'e', finalState);
-		}
-		return nfa; 
-	}
+        String startState = Integer.toString(stateCount++);
+        nfa.addStartState(startState);
+
+        String finalState = Integer.toString(stateCount++);
+        nfa.addFinalState(finalState);
+
+        nfa.addTransition(startState, c, finalState);
+
+        return nfa;
+    }
+
+    public NFA concatenate(NFA nfa, NFA nfa2) {
+        nfa.addAbc(nfa2.getABC());
+        for (State s : nfa.getFinalStates()) {
+            NFAState state = (NFAState) s;
+            state.setNonFinal();
+            state.addTransition('e', (NFAState) nfa2.getStartState());
+        }
+        nfa.addNFAStates(nfa2.getStates());
+        return nfa;
+    }
+
+    public NFA union(NFA nfa, NFA nfa2) {
+        NFAState nfaState = (NFAState) nfa.getStartState();
+        NFAState nfaState2 = (NFAState) nfa2.getStartState();
+
+        nfa.addNFAStates(nfa2.getStates());
+        nfa.addAbc(nfa2.getABC());
+
+        String startState = Integer.toString(stateCount++);
+        nfa.addStartState(startState);
+        String finalState = Integer.toString(stateCount++);
+        nfa.addFinalState(finalState);
+
+        nfa.addTransition(startState, 'e', nfaState.getName());
+        nfa.addTransition(startState, 'e', nfaState2.getName());
+
+        for (State s : nfa2.getFinalStates()) {
+            NFAState state = (NFAState) s;
+            state.setNonFinal();
+            nfa.addTransition(state.getName(), 'e', finalState);
+        }
+        return nfa;
+    }
 }
